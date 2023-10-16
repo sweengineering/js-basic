a4981c1f70572de34791a326c7bdc08332d911fe
Pekka Alaluukas <pekka.alaluukas@oamk.fi>
2023-10-16 09:35:42 +0300

    javascript examples

---
+++ ex1.js
@@ -0,0 +1,9 @@
+console.log("Hello");
+let firstname="Bob";
+console.log("Hello "+firstname);
+firstname="Lisa";
+console.log("Hello "+firstname);
+
+const lastname="Smith";
+
+console.log("Hello "+lastname);
---
+++ ex2.js
@@ -0,0 +1,6 @@
+function calculateSum(a, b){
+    return a+b;
+}
+console.log("The application starts");
+let result=calculateSum(5,7);
+console.log("Sum = "+result);
